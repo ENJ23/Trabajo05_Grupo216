15c2f70be5fb14fea8b038ad9fa5bb7339bbbfd3
Usuario <Usuario@DESKTOP-UAQ2Q4U>
2024-07-03 21:21:47 -0300

    Incializacion Proyecto rama master - master (Jarma Elias Nicolas)

---
+++ src/main/java/ar/edu/unju/fi/TrabajoP05Grupo216Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabajoP05Grupo216Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabajoP05Grupo216Application.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/unju/fi/TrabajoP05Grupo216ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabajoP05Grupo216ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
